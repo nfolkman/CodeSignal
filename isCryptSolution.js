8a37fa17aebb96e47f65d74609894c6c1c717c26
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-08-18 21:39:06 -0500

    Interview Prep > Arrays

--- isCryptSolution.js
+++ isCryptSolution.js
@@ -1,9 +1,19 @@
+/* Description: A cryptarithm is a mathematical puzzle for which the goal is to find the correspondence between letters and digits, 
+such that the given arithmetic equation consisting of letters holds true when the letters are converted to digits.
 
+You have an array of strings crypt, the cryptarithm, and an an array containing the mapping of letters and digits, solution. 
+The array crypt will contain three non-empty strings that follow the structure: [word1, word2, word3], which should be interpreted as the word1 + word2 = word3 cryptarithm.
+
+If crypt, when it is decoded by replacing all of the letters in the cryptarithm with digits using the mapping in solution, 
+becomes a valid arithmetic equation containing no numbers with leading zeroes, the answer is true. 
+If it does not become a valid arithmetic solution, the answer is false.
+
+Note that number 0 doesn't contain leading zeroes (while for example 00 or 0123 do).  */
 
 
 
 function solution(crypt, solution) {
-	// Attempt 1 : Incomplete
+	// Solution 1 : Incomplete
 	/* // crypt = array of 3 strings/words
 	// solution = a mapping of letters and digits
 	
@@ -23,7 +33,7 @@ function solution(crypt, solution) {
 	    }
 	  } */
 
-	// Attempt 2 : Complete (w/ help)
+	// Solution 2 : Complete (w/ help)
 
 	/* let hasLeadingZeros = false
 	
